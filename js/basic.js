8eca8827463bae7bd9dadab146ac438c4623670a
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2022-04-14 16:37:00 +0900

    add superpower function

--- js/basic.js
+++ js/basic.js
@@ -3,6 +3,9 @@ var ctx = canvas.getContext('2d');
 var rightPressed = false;
 var leftPressed = false;
 
+var goldshipImg = new Image();
+goldshipImg.src = "./media/goldship.png";
+
 var shipSize = 40;
 var shipImg = new Image();
 shipImg.src = "./media/ship.png";
@@ -10,13 +13,34 @@ var ship = {
     x: (canvas.width - shipSize) / 2,
     y: canvas.height - shipSize,
     w: shipSize,
-    h: shipSize
+    h: shipSize,
+    superPower: false
 };
 var enemySize = 40;
 var enemyImg = new Image();
 enemyImg.src = "./media/enemy.png";
 var enemyCount = 10;
 var enemyStatus = [];
+
+
+var heartSize = 100;
+var heartImg = new Image();
+heartImg.src = "./media/heart.png";
+var heartCount = 3;
+var heartStatus = [];
+
+for (var i = 0; i < heartCount; i++) {
+    heartStatus[i] = {
+        x: i * 30,
+        y: 0,
+        w: heartSize,
+        h: heartSize,
+        img: heartImg,
+        status: 0
+    }
+}
+
+
 for (var i = 0; i < enemyCount; i++) {
     enemyStatus[i] = {
         x: 0,
@@ -29,6 +53,9 @@ for (var i = 0; i < enemyCount; i++) {
 }
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+
+
+
 
 function keyDownHandler(e) {
     if (e.code == 'ArrowRight') {
@@ -45,14 +72,19 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-
+function superPower() {
+    ship.superPower = true;
+    setTimeout(function () { ship.superPower = false; }, 2000);
+}
 function checkCrash() {
     for (var i = 0; i < enemyCount; i++) {
         var enemy = enemyStatus[i];
         if (enemy.status == 0) {
             continue;
         }
-
+        if (ship.superPower) {
+            continue;
+        }
         ship.rx = ship.x + ship.w;
         ship.by = ship.y + ship.h;
         enemy.rx = enemy.x + enemy.w;
@@ -62,6 +94,8 @@ function checkCrash() {
             (ship.rx >= enemy.x && ship.rx <= enemy.rx)) {
             if ((ship.y >= enemy.y && ship.y <= enemy.by) ||
                 (ship.by >= enemy.y && ship.by <= enemy.by)) {
+                enemy.status = 0;
+                superPower();
                 return 1;
             }
         }
@@ -107,19 +141,69 @@ function drawAllEnemies() {
     createNewEnemy(30, 1);
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawAllEnemies();
-    ctx.drawImage(shipImg, ship.x, ship.y, ship.w, ship.h);
+function checkheart() {
+    var count = 0;
+    for (var i = 0; i < heartCount; i++) {
+        if (heartStatus[i].status == 1) {
+            count += 1;
+        }
+    }
+    if (count == heartCount) {
+        return 1;
+    }
+    return 0;
+}
 
+
+function deleteHeart() {
+    for (var i = 0; i < enemyCount; i++) {
+        if (heartStatus[i].status == 0) {
+            heartStatus[i].status = 1;
+            return;
+        }
+    }
+}
+
+function drowHeart() {
+    for (var i = 0; i < heartCount; i++) {
+        if (heartStatus[i].status == 0) {
+            ctx.drawImage(heartStatus[i].img, heartStatus[i].x, heartStatus[i].y, heartStatus[i].w, heartStatus[i].h);
+        }
+    }
+
+}
+
+
+function drowShip() {
+    if (ship.superPower) {
+        ctx.drawImage(goldshipImg, ship.x, ship.y, ship.w, ship.h);
+        // continue;
+    }
+    else {
+        ctx.drawImage(shipImg, ship.x, ship.y, ship.w, ship.h);
+    }
     if (rightPressed && ship.x < canvas.width - shipSize) {
         ship.x += 10;
     } else if (leftPressed && ship.x > 0) {
         ship.x -= 10;
     }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawAllEnemies();
+    drowHeart();
+    drowShip();
+    if (checkheart()) {
+        return 1;
+    }
     if (checkCrash()) {
+        deleteHeart();
         ctx.fillText("Crash!!", 10, 20);
     }
+
     requestAnimationFrame(draw);
 }
+
+
 draw();
